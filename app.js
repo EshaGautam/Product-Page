eb417c15f408ae7f06ce488aee45b3dbe17ddceb
Esha Gautam <eshagautam0@gmail.com>
2024-07-13 21:19:28 +0530

    added user and product relationship

--- app.js
+++ app.js
@@ -1,36 +1,58 @@
 const path = require('path');
-
-const sequelize = require('./util/database')
-
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const sequelize = require('./util/database');
+const User = require('./models/user');
+const Product = require('./models/product');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
-
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
-
-
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then((user) => {
+            req.user = user;
+            next();
+        })
+        .catch(err => {
+            console.log(err);
+          
+        });
+});
+
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.get404);
 
-sequelize.sync().then(()=>{
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
 
-    app.listen(4000);
-})
-.catch(err=>console.log(err))
-
+sequelize.sync()
+    .then(() => {
+        return User.findByPk(1);
+    })
+    .then((user) => {
+        if (!user) {
+            return User.create({ id:1,name: 'isha', email: 'isha111@gmail.com' });
+        }
+        return user;
+    })
+    .then((user) => {
+        console.log('Connected to database and user found or created:', user);
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+    })
+    .catch(err => {
+        console.error('Error initializing server:', err);
+    });
--- controllers/admin.js
+++ controllers/admin.js
@@ -16,15 +16,16 @@ exports.postAddProduct = (req, res, next) => {
   const imageURL = req.body.imageURL;
   const price = req.body.price;
   const description = req.body.description;
-
-
-  Product.create({
-    title:title,
-    imageURL:imageURL,
-    price:price,
-    description:description,
-  })
-  .then((result)=>console.log(result))
+ req.user.createProduct({
+  title:title,
+  imageURL:imageURL,
+  price:price,
+  description:description,
+})
+  .then((result)=>{
+    console.log(result)
+    res.redirect('/')
+})
   .catch((err)=>console.log(err))
 
 };
@@ -35,10 +36,14 @@ exports.addEditProduct=(req,res,next)=>{
      return res.redirect('/')
   }
   const prodId = req.params.productId
-  Product.findByPk(prodId).then((product)=>{
-    if(!product){
+  req.user.getProducts({where:{id:prodId}})
+  // Product.findByPk(prodId)
+  .then((products)=>{
+    const product = products[0]
+    if(!products){
       return res.redirect('/')
     }
+  
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
